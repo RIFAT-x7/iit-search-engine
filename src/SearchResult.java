ca2d4687ca55e1b5aefe259c85e88f83d15356bc
Rifat Al Maruf <bsse1705@iit.du.ac.bd>
2026-09-23 23:11:08 +0600

    feat: add search result model

---
+++ src/SearchResult.java
@@ -0,0 +1,12 @@
+public class SearchResult {
+    private final Document document;
+    private final double score;
+
+    public SearchResult(Document document, double score) {
+        this.document = document;
+        this.score = score;
+    }
+
+    public Document getDocument() { return document; }
+    public double getScore() { return score; }
+}
